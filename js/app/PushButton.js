e2d7cb1345d112031142c6ea9a3a33293afbde6c
Alec Cove <alec.cove@gmail.com>
2016-10-19 13:32:16 -0400

    Add first component

---
+++ js/app/PushButton.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+/*
+ *
+ */
+define(function (require) {
+
+
+    var AssetLoader = require('app/AssetLoader');
+
+
+    var PushButton = function (assetFile, container) {
+
+        var assetLoader = new AssetLoader();
+
+    }
+
+
+    return PushButton;
+});
