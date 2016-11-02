05b7712551494be094f74d477899ced4457d531b
Alec Cove <alec.cove@gmail.com>
2016-11-02 12:40:34 -0400

    Complete basic production fields

--- js/app/ProductionInput.js
+++ js/app/ProductionInput.js
@@ -5,20 +5,44 @@
  * An input field with an add or remove button at the end. Allows user to enter an l-system
  * production value. Handles it's own siblings, adding or removing them.
  */
-define(function (require) {
+define(function () {
+
+
+    var MAX_FIELDS = 4;
 
 
     var ProductionInput = function (elementId) {
 
+        this.fieldCount = 1;
         this.element = document.getElementById(elementId);
         this.elementBtn = this.element.getElementsByClassName("prod-btn")[0];
-        this.elementBtn.addEventListener("click", this.buttonDown.bind(this));
+        this.elementBtn.addEventListener("click", this.cloneElement.bind(this));
     }
 
 
-    ProductionInput.prototype.buttonDown = function() {
-        // determine the type of button (add or remove)
-        console.log(this.element.className)
+    /*
+     * Event handler for "add" button clicks. Creates a cloned field and prepends it to the DOM
+     */
+    ProductionInput.prototype.cloneElement = function() {
+
+        if (this.fieldCount >= MAX_FIELDS) return;
+
+        var clone = this.element.cloneNode(true);
+        clone.getElementsByClassName("button-label")[0].innerHTML = "remove";
+        clone.addEventListener("click", this.removeElement.bind(this));
+        this.element.parentElement.insertBefore(clone, this.element);
+
+        this.fieldCount++
+    }
+
+
+    /*
+     * Event handler for "remove" buttons - removes field from the DOM and decrements field count
+     */
+    ProductionInput.prototype.removeElement = function(evt) {
+        var elem = evt.target.parentElement.parentElement;
+        elem.parentElement.removeChild(elem);
+        this.fieldCount--;
     }
 
 
