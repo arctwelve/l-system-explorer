409038010b8082ad1debce8c81e297dc4ed02163
Alec Cove <alec.cove@gmail.com>
2016-11-27 16:09:17 -0500

    Integrate Slider events

--- js/app/Slider.js
+++ js/app/Slider.js
@@ -9,15 +9,99 @@ define(function () {
 
         this.element = document.getElementById(elementID);
         this.elementBtn = this.element.getElementsByClassName("small-slider-knob")[0];
-        this.elementBtn.addEventListener("click", this.clickBtn.bind(this));
+        this.elementBtn.addEventListener("mousedown", this.knobMouseDown.bind(this));
+        /*
+        this.elementBtn.onmousedown = function() {
+
+            var self = this;
+
+            document.onmousemove = function(e) {
+                self.style.left = e.clientX - 46 + 'px';
+            };
+
+            this.onmouseup = function() {
+                document.onmousemove = null;
+            };
+
+            document.onmouseup = function() {
+                document.onmousemove = null;
+            };
+        };
+
+        this.elementBtn.ondragstart = function() {
+            return false;
+        };
+        */
     };
 
 
-    Slider.prototype.clickBtn = function () {
-        this.elementBtn.style.left = "100px";
+    Slider.prototype.knobMouseDown = function(e) {
+
+        document.onmouseup = this.knobMouseUp.bind(this);
+        document.onmousemove = this.knobMouseMove.bind(this);
+        this.elementBtn.onmouseup = this.knobMouseUp.bind(this);
+
+        this.elementBtn.ondragstart = function() {
+            return false;
+        };
     };
 
+
+    Slider.prototype.knobMouseMove = function(e) {
+        this.elementBtn.style.left = e.clientX - 46 + 'px';
+    };
+
+
+    Slider.prototype.knobMouseUp = function(e) {
+        document.onmousemove = null;
+        this.elementBtn.onmouseup = null;
+    };
 
 
     return Slider;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+document.getElementById('ball2').onmousedown = function() {
+  this.style.position = 'absolute'
+
+  var self = this
+
+  document.onmousemove = function(e) {
+    e = e || event
+    fixPageXY(e)
+
+    self.style.left = e.pageX-25+'px'
+    self.style.top = e.pageY-25+'px'
+  }
+  this.onmouseup = function() {
+    document.onmousemove = null
+  }
+}
+
+document.getElementById('ball2').ondragstart = function() { return false }
+*/
